ba57ce098eb82dfcf2da8b74e3807dca4a805945
Владимир Пайор <>
2020-04-13 17:12:48 +0300

    добавил log

--- config.py
+++ config.py
@@ -45,22 +45,8 @@ class ProductionConfig(Config):
 
         # email errors to the administrators
         import logging
-        from logging.handlers import SMTPHandler
-        credentials = None
-        secure = None
-        if getattr(cls, 'MAIL_USERNAME', None) is not None:
-            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-            if getattr(cls, 'MAIL_USE_TLS', None):
-                secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.FLASKY_MAIL_SENDER,
-            toaddrs=[cls.FLASKY_ADMIN],
-            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
-            credentials=credentials,
-            secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+        logging.basicConfig(filename='food.log', level=logging.DEBUG)
+
 
 config = {
     'development': DevelopmentConfig,
