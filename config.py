300d617b70386ef0e2c090c7fc0a6b2249874b3d
Владимир Пайор <>
2020-04-16 14:45:15 +0300

    проверка фикса роутинга 1.0

--- config.py
+++ config.py
@@ -41,7 +41,8 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI =  "mysql+pymysql://minecite:DBspmi-20@localhost/foods"
     LOGIN_VIEW = "https://digital.spmi.ru/mining_foods/auth/login"
-    SERVER_NAME = "https://digital.spmi.ru/mining_foods/"
+    SERVER_NAME = "https://digital.spmi.ru/"
+    PREFERRED_URL_SCHEME = "https"
     APPLICATION_ROOT = "/mining_foods"
 
     @classmethod
