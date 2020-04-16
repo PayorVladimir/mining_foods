8f89f3436058ec555c0743a2ed8e1b719c122ab4
Владимир Пайор <>
2020-04-16 17:00:01 +0300

    поправил роутинги для всех страниц и методов

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -51,25 +51,24 @@ def create_admin():
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.moderator', _external=True))
+        return redirect(url_for('main.moderator'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(login=form.username.data).first()
         if user is None or not user.verify_password(form.password.data):
             flash('Неверное имя пользователя или пароль')
-            return redirect("https://digital.spmi.ru/mining_foods/auth/login")
+            return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
-        # next_page = request.args.get('next')
-        # if not next_page or url_parse(next_page).netloc != '':
-        #     next_page = "https://digital.spmi.ru/mining_foods/"
-        return redirect(url_for('main.moderator'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.moderator')
+        return redirect(next_page)
     return render_template('login.html', title='Вход', form=form)
 
 
 @auth.route('/logout')
 def logout():
     logout_user()
-    return redirect("https://digital.spmi.ru/mining_foods/auth/login")
 
 
 @auth.route("/users", methods=['GET','POST'])
@@ -80,6 +79,6 @@ def users():
         user = User(login=form.login.data, username=form.username.data, password=form.password2.data )
         db.session.add(user)
         db.session.commit()
-        return redirect("https://digital.spmi.ru/mining_foods/auth/users/")
+        return redirect(url_for('main.moderator'))
 
     return render_template('users.html', form = form)
