2a6847b833b23012d9a29f72f9d450b2727cf98a
Владимир Пайор <>
2020-05-19 01:27:44 +0300

    Апдейт бэка

--- app/__init__.py
+++ app/__init__.py
@@ -52,7 +52,7 @@ def create_app(config_name):
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+        response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         response.headers.add('Access-Control-Allow-Credentials', 'true')
