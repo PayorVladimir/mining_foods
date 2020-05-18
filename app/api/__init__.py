40dba1362151a70c1d7cb0fc236fce8f1d56f315
Владимир Пайор <>
2020-05-19 00:59:08 +0300

    Апдейт бэка

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from ..models import Permission
-api = Blueprint('api', __name__)
+from flask_cors import CORS
 
+api = Blueprint('api', __name__)
+CORS(api, esources={r"/*": {"origins": "*"}}, headers='Content-Type')
 @api.app_context_processor
 def inject_permissions():
     return dict(Permission=Permission)
