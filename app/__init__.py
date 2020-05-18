bca4dcec9d88f883f709b9b33c2a42c792ea710c
Владимир Пайор <>
2020-05-19 00:05:43 +0300

    Апдейт бэка

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ mail = Mail()
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     if request.method == 'OPTIONS':
-        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT, PATCH'
         headers = request.headers.get('Access-Control-Request-Headers')
         if headers:
             response.headers['Access-Control-Allow-Headers'] = headers
